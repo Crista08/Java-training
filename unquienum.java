9325f66c40ab99ac67d9841daca6530719e5e05a
Crista08 <cristajospin2005@gmail.com>
2025-02-17 15:25:37 +0530

    Add files via upload

---
+++ unquienum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class unquienum{
+    public static void main(String[] args) {
+       
+        String numStr="123465788997564567123";
+        int[] count=new int[10];
+       
+        for (char digit:numStr.toCharArray()) {
+            count[digit-'0']++;
+        }
+       
+       
+        for (int i=0; i<10;i++) {
+            if (count[i]>0) {
+                System.out.print(i+":"+count[i]+" ");
+                System.out.println();
+            }
+        }
+    }
+}
