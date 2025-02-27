9e38b09ff87991080f654f3372e603cc08f9bf0f
Crista08 <cristajospin2005@gmail.com>
2025-02-27 12:50:55 +0530

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Binary
+{
+  public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    System.out.print("Enter the value of temp : ");
+    int temp=in.nextInt();
+    int rem;
+    String binary="";
+    while(temp>0)
+    {
+      rem=temp%2;
+      binary=rem+binary;
+      temp=temp/2;
+    }
+    System.out.println(binary);
+  }
+}
