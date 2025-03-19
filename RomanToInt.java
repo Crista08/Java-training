fecaa91f1498521b4c0bb37769f492def04c267f
Crista08 <cristajospin2005@gmail.com>
2025-03-19 16:18:14 +0530

    RomanToInt.java

--- RomanToInt.java
+++ RomanToInt.java
@@ -2,19 +2,22 @@ class RomanToInt
 {
     public static int romanToInt(String s)
     {
-      int a=0;
-        int[] b={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-        for(int i=0;i<symbols.length;i++)
-        {
-            while(s.indexOf(symbols[i])==0)
-            {
-                a+=b[i];
-                s=s.substring(symbols[i].length());
-            }
-        }
-        return a;
+		int a=0;
+		int[] b= {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+		String[] symbols= {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		int i=0;
+		while(i<s.length()) {
+			for(int j=0; j<symbols.length; j++)
+			{
+				if(s.startsWith(symbols[j],i))
+				{
+					a+=b[j];
+					i+=symbols[j].length();
+					break;
+				}
+			}
+		}
+			return a;
     }
     public static void main(String[] args)
     {
@@ -22,5 +25,7 @@ class RomanToInt
         System.out.println(romanToInt("LVIII"));
         System.out.println(romanToInt("MCMXCIV"));
         System.out.println(romanToInt("CDXCIV"));
+        System.out.println(romanToInt("IIXCDXC"));
+        System.out.println(romanToInt("LVCMIV"));
     }
 }
