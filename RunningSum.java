a58b494d4e1ed10bcd7c290cb62e546540d208fb
Crista08 <cristajospin2005@gmail.com>
2025-07-25 12:23:40 +0530

    Add files via upload

---
+++ RunningSum.java
@@ -0,0 +1,14 @@
+class RunningSum{
+    public static void main(String args[]){
+        int [] arr={1,2,3,4};
+int[] result=new int[arr.length];
+int total=0;
+for(int i=0;i<arr.length;i++){
+    total+=arr[i];
+    result[i]=total;
+}
+for(int num:result){
+System.out.println(num);
+    }
+}
+}
